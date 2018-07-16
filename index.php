57d8ff47e47740d1e2f2e744a4f9d0590e36ed1b
bwl <zyczynski7@gmail.com>
2018-07-16 18:32:54 +0200

    tak sobie kombinuje
    Czy to w dobrą stronę zmierza, czy zupełnie nie tak...
    print_r niby spoko, ale przy echowaniu Notice: Array to string conversion...
    jakaś podpowiedz?

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 require_once 'produkt.php';
 
 
-$jablko = new Produkt('jablko', '5');
-$bombonierka = new Produkt('bombonierka','10');
+    $produkt[1] = [ new Produkt('jablko', '5')];
+    $produkt[2] = [ new Produkt('bombonierka','10')];
+    $produkt[3] = [ new Produkt('cebula','2')];
+    $produkt[4] = [ new Produkt('flaszka','50')];
+    $produkt[5] = [ new Produkt('zupa','3')];
+    $produkt[6] = [ new Produkt('marchew','3')];
+    $produkt[7] = [ new Produkt('marichuanina','40')];
+//echo "</br>";
+//echo "a" . $bombonierka->getNazwa() . " kosztuje " . $bombonierka->getCena();
 
-echo $jablko->getNazwa() ." kosztuje " . $jablko->getCena();
-echo "</br>";
-echo "a" . $bombonierka->getNazwa() . " kosztuje " . $bombonierka->getCena();
+foreach ($produkt as $klucz => $wartosc)
+{
+    echo ($produkt);
+}
